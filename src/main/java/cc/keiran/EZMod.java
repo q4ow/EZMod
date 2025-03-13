52dedb5c7ddb28bf1c269f308190ab495d110d99
Keiran <keiran0@proton.me>
2025-03-13 23:48:01 +0000

    finished implementing command aliases.

--- src/main/java/cc/keiran/EZMod.java
+++ src/main/java/cc/keiran/EZMod.java
@@ -105,20 +105,35 @@ public class EZMod {
 
     private void registerCommandAliases() {
         commandAliases = new HashMap<>();
-        commandAliases.put("!f1", "/warp dungeon_hub");
-        commandAliases.put("!f2", "/warp dungeon_hub");
-        commandAliases.put("!f3", "/warp dungeon_hub");
-        commandAliases.put("!f4", "/warp dungeon_hub");
-        commandAliases.put("!f5", "/warp dungeon_hub");
-        commandAliases.put("!f6", "/warp dungeon_hub");
-        commandAliases.put("!f7", "/warp dungeon_hub");
+        commandAliases.put("!d", "/warp dungeon_hub");
+        commandAliases.put("!c", "/collections");
+        commandAliases.put("!f1", "/joininstance CATACOMBS_FLOOR_ONE");
+        commandAliases.put("!f2", "/joininstance CATACOMBS_FLOOR_TWO");
+        commandAliases.put("!f3", "/joininstance CATACOMBS_FLOOR_THREE");
+        commandAliases.put("!f4", "/joininstance CATACOMBS_FLOOR_FOUR");
+        commandAliases.put("!f5", "/joininstance CATACOMBS_FLOOR_FIVE");
+        commandAliases.put("!f6", "/joininstance CATACOMBS_FLOOR_SIX");
+        commandAliases.put("!f7", "/joininstance CATACOMBS_FLOOR_SEVEN");
+        commandAliases.put("!m1", "/joininstance MASTER_CATACOMBS_FLOOR_ONE");
+        commandAliases.put("!m2", "/joininstance MASTER_CATACOMBS_FLOOR_TWO");
+        commandAliases.put("!m3", "/joininstance MASTER_CATACOMBS_FLOOR_THREE");
+        commandAliases.put("!m4", "/joininstance MASTER_CATACOMBS_FLOOR_FOUR");
+        commandAliases.put("!m5", "/joininstance MASTER_CATACOMBS_FLOOR_FIVE");
+        commandAliases.put("!m6", "/joininstance MASTER_CATACOMBS_FLOOR_SIX");
+        commandAliases.put("!m7", "/joininstance MASTER_CATACOMBS_FLOOR_SEVEN");
+
     }
 
     @SubscribeEvent
     public void onChatReceived(ClientChatReceivedEvent event) {
-        String message = event.message.getUnformattedText();
-        if (commandAliases.containsKey(message)) {
-            Minecraft.getMinecraft().thePlayer.sendChatMessage(commandAliases.get(message));
+        String message = event.message.getUnformattedText().trim();
+        String[] parts = message.split(" ", 2);
+        String commandAlias = parts.length > 1 ? parts[1] : parts[0];
+
+        if (commandAliases.containsKey(commandAlias)) {
+            event.setCanceled(true);
+            String command = commandAliases.get(commandAlias);
+            Minecraft.getMinecraft().thePlayer.sendChatMessage(command);
         }
     }
 
@@ -407,7 +422,7 @@ public class EZMod {
         StringBuilder response = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(stream, StandardCharsets.UTF_8))) {
-            String line;
+            String line = "";
             while ((line != null)) {
                 response.append(line);
             }
