9f291b62368b2efda4d5bac04f5b1e46b8868351
Keiran <keiran0@proton.me>
2025-03-19 21:51:46 +0000

    testing some client-side chat ui stuff

--- src/main/java/cc/keiran/EZMod.java
+++ src/main/java/cc/keiran/EZMod.java
@@ -1,10 +1,12 @@
 package cc.keiran;
 
 import cc.keiran.commands.CommandAliasHandler;
-import cc.keiran.commands.EZModCommands; // Updated import
+import cc.keiran.commands.EZModCommands;
 import cc.keiran.config.EZModConfig;
 import cc.keiran.screenshot.ScreenshotManager;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
@@ -12,7 +14,6 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
-import net.minecraftforge.client.ClientCommandHandler;
 import org.lwjgl.input.Keyboard;
 
 @Mod(modid = EZMod.MODID, name = EZMod.NAME, version = EZMod.VERSION)
@@ -25,12 +26,11 @@ public class EZMod {
 
     public static KeyBinding screenshotKey;
     private ScreenshotManager screenshotManager;
-    private CommandAliasHandler commandAliasHandler;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         EZModConfig.init(event.getModConfigurationDirectory());
-        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(new ModEventHandler());
     }
 
     @Mod.EventHandler
@@ -38,19 +38,31 @@ public class EZMod {
         screenshotKey = new KeyBinding("Take Screenshot", Keyboard.KEY_F12, "EZMod");
         ClientRegistry.registerKeyBinding(screenshotKey);
 
-        screenshotManager = new ScreenshotManager();
-
         ClientCommandHandler.instance.registerCommand(new EZModCommands());
 
-        commandAliasHandler = new CommandAliasHandler();
+        CommandAliasHandler commandAliasHandler = new CommandAliasHandler();
         MinecraftForge.EVENT_BUS.register(commandAliasHandler);
     }
 
     @SubscribeEvent
     public void onKeyInput(InputEvent.KeyInputEvent event) {
         if (screenshotKey.isPressed()) {
-            screenshotManager.takeAndUploadScreenshot();
+            takeScreenshot();
+        }
+    }
+
+    private void takeScreenshot() {
+        if (screenshotManager == null) {
+            screenshotManager = new ScreenshotManager();
+        }
+        screenshotManager.takeAndUploadScreenshot();
+    }
+
+    private static class ModEventHandler {
+        @SubscribeEvent
+        public void onClientChatReceived(ClientChatReceivedEvent event) {
+            String message = event.message.getFormattedText();
+            event.message = new net.minecraft.util.ChatComponentText(EZMod.PREFIX + message);
         }
     }
 }
-
